3914d0c723fd8788b9d7508e65992ac2b58c4d99
Ana <eaaania@students.eaaa.dk>
2020-09-15 11:33:38 +0200

    firebase init
    firebase works, but we still need to add a lot of stuff

--- js/main.js
+++ js/main.js
@@ -1,5 +1,27 @@
 "use strict";
+const firebaseConfig = {
+    apiKey: "AIzaSyAR47yn3G9m50zR_IVG1AIzGqJQoK30qro",
+    authDomain: "throwmeright.firebaseapp.com",
+    databaseURL: "https://throwmeright.firebaseio.com",
+    projectId: "throwmeright",
+    storageBucket: "throwmeright.appspot.com",
+    messagingSenderId: "383862855912",
+    appId: "1:383862855912:web:3476d0165885af3542115d"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const _db = firebase.firestore();
+const _categoryRef = _db.collection("categories");
+let _categories = [];
+_categoryRef.onSnapshot(function (snapshotData) {
 
+    snapshotData.forEach(function (doc) {
+        let category = doc.data();
+        category.id = doc.id;
+        _categories.push(category);
+    });
+});
+console.log(_categories);
 //Open more section in nav - Wojo
 let moreBtn = document.querySelector(".moreBtn");
 let navMore = document.querySelector(".nav-more");
@@ -8,7 +30,7 @@ let nav = document.querySelector("nav");
 
 
 function toggleMore() {
-    
+
     if (navMore.style.bottom > "15%") {
         return closeMore();
     }
