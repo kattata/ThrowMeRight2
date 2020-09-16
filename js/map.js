97b5c1a031e93b450d81ea1e75f822f063daed77
Kasia Laniecka <kasia.laniecka@gmail.com>
2020-09-16 10:32:30 +0200

    slick in map.html works!!

--- js/map.js
+++ js/map.js
@@ -1,8 +1,7 @@
 $(document).ready(function () {
     $('.map-categories').slick({
-        infinite: true,
         slidesToShow: 3,
-        slidesToScroll: 3
+        slidesToScroll: 2
     });
 });
 
