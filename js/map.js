a3623d4ac1c348ffb7068bf8b5a38432f6b3f944
Kasia Laniecka <kasia.laniecka@gmail.com>
2020-09-16 11:47:11 +0200

    mappp

--- js/map.js
+++ js/map.js
@@ -6,9 +6,12 @@ $(document).ready(function () {
 });
 
 mapboxgl.accessToken = 'pk.eyJ1Ijoia2F0dGF0YSIsImEiOiJjazdkMW9samkwamVxM2ZwYTdycWVqeTdnIn0.UiaVPV8_C6knWwC1_K8zkA';
-var map = new mapboxgl.Map({
-    container: 'map',
+let map = new mapboxgl.Map({
+    container: 'mapbox',
     style: 'mapbox://styles/kattata/ckf3tp6w60wkg19ql7zv06r0y',
-    center: [0, 0],
-    zoom: 5
+    center: [10.195, 56.154,],
+    zoom: 11
 });
+
+
+
