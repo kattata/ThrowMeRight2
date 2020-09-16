320d47ce3e5481639b11ce85ed9cbb4699fccddb
Kasia Laniecka <kasia.laniecka@gmail.com>
2020-09-16 10:45:50 +0200

    transfered map functionality to index.html

--- js/map.js
+++ js/map.js
@@ -5,3 +5,10 @@ $(document).ready(function () {
     });
 });
 
+mapboxgl.accessToken = 'pk.eyJ1Ijoia2F0dGF0YSIsImEiOiJjazdkMW9samkwamVxM2ZwYTdycWVqeTdnIn0.UiaVPV8_C6knWwC1_K8zkA';
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/kattata/ckf3tp6w60wkg19ql7zv06r0y',
+    center: [0, 0],
+    zoom: 5
+});
